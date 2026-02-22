1274ad09a92edb9c77ce96bd11ab23174c2f9191
Tz055 <zzhilyaev@gmail.com>
2026-02-22 12:57:19 +0300

    Фикс логина

--- flower_shop/shop/urls.py
+++ flower_shop/shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('', views.catalog, name='catalog'),
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='shop/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='catalog'), name='logout'),
+    path('login/', LoginView.as_view(template_name='shop/login.html', next_page=reverse_lazy('catalog')), name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('catalog')), name='logout'),
     path('add/', views.add_flower, name='add_flower'),
 ]
